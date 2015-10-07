9c41b1c5eb8c883ccc1d6fb74df73ea506e93f51
QRayarch <afl4947@rit.edu>
2015-10-07 13:35:37 -0400

    Worked on the Grid system. Currently can resize and it resizes the grid for you. Working on adding brushes so you can quickly add sprites to the grid.

---
+++ Assets/Editor/CustomInspector/GridEditor.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using System.Collections;
+using System.Collections.Generic;
+
+[CustomEditor(typeof(Grid))]
+[CanEditMultipleObjects]
+public class GridEditor : Editor {
+
+	private GUIStyle style = new GUIStyle();
+
+	private ReorderableList list;
+
+	private void OnEnable() {
+		list = new ReorderableList(serializedObject, serializedObject.FindProperty("brushes")
+		                           , false, true, true, true);
+		list.drawElementCallback += DrawBrush;
+		list.onSelectCallback += SelectElemet;
+
+	}
+
+
+	private void DrawBrush(Rect rect, int index, bool isActive, bool isFocused) {
+		SerializedProperty brush = list.serializedProperty.GetArrayElementAtIndex(index);
+		EditorGUI.PropertyField(rect, brush.FindPropertyRelative("sprite"));
+		//style.normal.background = brush.FindPropertyRelative("sprite");
+	}
+
+	private void SelectElemet(ReorderableList l) {
+		Grid grid = target as Grid;
+		grid.selectedBrushIndex = l.index;
+		Debug.Log(grid.selectedBrushIndex);
+	}
+
+	public override void OnInspectorGUI() {
+		serializedObject.Update();
+		list.DoLayoutList();
+		serializedObject.ApplyModifiedProperties();
+
+		//Change the UI
+		if(GUI.changed) {
+			EditorUtility.SetDirty(target);
+		}
+	}
+
+	void OnSceneGUI() {
+		Event e = Event.current;
+		Grid grid = target as Grid;
+
+		switch(e.type) {
+			case EventType.MouseUp :
+				if(e.button == 0) {
+					Vector2 pos = e.mousePosition;
+					pos = SceneView.currentDrawingSceneView.camera.ScreenToWorldPoint(pos);
+					grid.AddGridSpaceFromMousePos(pos);
+				}
+				break;
+		}
+	}
+}
---
+++ Assets/Scripts/Grid/Grid.cs
@@ -0,0 +1,171 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(RectTransform))]
+public class Grid : MonoBehaviour {
+
+	//Stuff for brushes
+	[Header("Tile Brushes")]
+	public List<GridBrush> brushes = new List<GridBrush>();
+	public int selectedBrushIndex;
+
+	[System.Serializable]
+	public class GridBrush {
+		public Sprite sprite;
+
+		public bool HasSprite {
+			get { return sprite != null; }
+		}
+	}
+
+	//General stuff
+
+	private GridSpace[,] spaces;
+
+	private Vector2 oldSize;
+	private Transform trans;
+	private RectTransform recT;
+
+	// Use this for initialization
+	void Start () {
+		trans = transform;
+		recT = GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckChangeSize();
+	}
+
+	private void CheckChangeSize() {
+		Vector2 size = recT.sizeDelta;
+		size.x = Mathf.Max(0, Mathf.Round(size.x));
+		size.y = Mathf.Max(0, Mathf.Round(size.y));
+		recT.sizeDelta = size;
+		if(size != oldSize || spaces == null) {
+			ChangeSize((int)size.x, (int)size.y);
+			Vector3 newPos = trans.position;
+			newPos.y = size.y;
+			trans.position = newPos;
+		}
+		oldSize = size;
+	}
+
+	private void ChangeSize(int newW, int newH) {
+		int oldW = GridWidth;
+		int oldH = GridHeight;
+
+		//If nothing changed why change things? Just quit
+		if(newW == oldW && newH == oldH) return;
+
+		if(spaces != null) {
+			GridSpace[,] oldSpaces = spaces;//Save old Grid
+			spaces = new GridSpace[newW, newH];//Set up new Grid
+			
+			//Copy over new Squares
+			for(int x = 0; x < newW; x++) {
+				for(int y = 0; y < newH; y++) {
+					//If things are getting bigger don't copy
+					if(x < oldW && y < oldH) {
+						spaces[x, y] = oldSpaces[x, y];
+					}
+				}
+			}
+		} else {
+			spaces = new GridSpace[newW, newH];//Set up new Grid
+		}
+	}
+
+	public void AddGridSpaceFromMousePos(Vector2 mousePos) {
+
+		Debug.Log(mousePos);
+
+		AddGridSpace(GetBoundedXCortinate(mousePos.x), 
+		             GetBoundedYCortinate(mousePos.y), 
+		             brushes[selectedBrushIndex]);
+	}
+
+	public bool AddGridSpace(int x, int y, GridBrush brush) {
+		if(!brush.HasSprite) {
+			RemoveGridSpace(x, y);
+			return true;
+		}
+		if(spaces[x, y] != null) return false;
+		GameObject newSpace = new GameObject();
+		newSpace.name = "(" + x + ", " + y + ")";
+
+		Transform newSpaceT = newSpace.transform;
+		newSpaceT.SetParent(trans, false);
+		Vector3 center = brush.sprite.bounds.center;
+		newSpaceT.position = new Vector3(x + center.x / 2, GridHeight - y + center.y / 2, 0.0f);
+		float width = 1.0f / brush.sprite.bounds.size.x;
+		float height = 1.0f / brush.sprite.bounds.size.y;
+		newSpaceT.localScale = new Vector3(width, height, 1.0f);
+
+		newSpace.AddComponent<GridSpace>();
+
+		SpriteRenderer spriteRender =  newSpace.AddComponent<SpriteRenderer>();
+		spriteRender.sprite = brush.sprite;
+
+		return true;
+	}
+
+	public void RemoveGridSpace(int x, int y) {
+		GridSpace deletedSpace = spaces[x, y];
+		if(deletedSpace == null) return;
+		spaces[x, y] = null;
+		Destroy(deletedSpace.gameObject);
+	}
+
+	public int GetBoundedXCortinate(float x) {
+		return Mathf.Min(GridWidth - 1,  Mathf.Max(0, (int)Mathf.Round(x)));
+	}
+
+	public int GetBoundedYCortinate(float y) {
+		return 	Mathf.Min(GridHeight - 1,  Mathf.Max(0, (int)Mathf.Round(y)));
+	}
+
+	#region Editor
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.white;
+		DrawGrid();
+	}
+	
+	void OnDrawGizmosSelected() {
+		Gizmos.color = Color.green;
+		DrawGrid();
+	}
+	
+	private void DrawGrid() {
+		Vector3 temp;
+		for(int x = 0; x <= GridWidth; x++) {
+			temp = trans.position + Vector3.right * x;
+			Gizmos.DrawLine(temp, temp + Vector3.down * GridHeight);
+		}
+		for(int y = 0; y <= GridHeight; y++) {
+			temp = trans.position + Vector3.down * y;
+			Gizmos.DrawLine(temp + Vector3.right * GridWidth, temp);
+		}
+	}
+
+	#endregion
+
+	//Properties
+
+	public int GridWidth {
+		get {
+			if(spaces == null) return - 1;
+			return spaces.GetLength(0); 
+		}
+	}
+
+	public int GridHeight {
+		get{ 
+			if(spaces == null) return - 1;
+			return spaces.GetLength(1); 
+		}
+	}
+}
