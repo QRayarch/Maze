dceb3ff76d888f59c79cf1c7020626d69f39401e
Michael Mullin <mvm5854@rit.edu>
2015-10-08 09:13:21 -0400

    started movement scripts

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : Move
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetAxis ("Horizontal") < 0) {
+			moveLeft();
+		}
+
+		if (Input.GetAxis ("Horizontal") > 0) {
+			moveRight();
+		}
+
+		if (Input.GetAxis ("Jump") > 0) {
+			jump();
+		}
+	}
+}
+
