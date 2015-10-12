b5d4b0177b1d0850b9eda6597d6cf6538181ab4d
unknown <mvm5854@GDD-31.main.ad.rit.edu>
2015-10-12 16:26:54 -0400

    started grid movement

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -1,23 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//http://answers.unity3d.com/questions/584736/method-for-grid-movement.html
+
 public class Move : MonoBehaviour {
 
 	// TODO: Change to grid based movement
+	public float speed=1.0f;
 
-	protected void moveLeft() {
-		float xMovement = Input.GetAxis ("Horizontal");
-		transform.position += new Vector3 (xMovement, 0, 0);
+	private Vector3 endPos;
+	private bool moving=false;
+
+	void Start()
+	{
+		endPos = transform.position;
+	}
+
+	protected void move(float dir)
+	{
+		if (moving && (transform.position == endPos))
+			moving = false;
+
+		if (!moving) {
+
+			if (dir > 0) {
+				moving=true;
+				endPos=transform.position + new Vector3 (1, 0, 0);
+			} 
+
+			else if (dir < 0) {
+				moving=true;
+				endPos=transform.position - new Vector3 (1, 0, 0);
+			}
+		}
+
+		transform.position = Vector3.MoveTowards (transform.position, endPos, Time.deltaTime * speed);
+	}
+
+	/*protected void moveLeft() {
+		//float xMovement = Input.GetAxis ("Horizontal");
+		transform.position -= new Vector3 (.1f, 0, 0);
 	}
 
 	protected void moveRight() {
 		//float xMovement = Input.GetAxis ("Horizontal");
 		//transform.position += new Vector3 (xMovement, 0, 0);
-		transform.position += new Vector3 (.016666667f, 0, 0);
-	}
+		transform.position += new Vector3 (.1f, 0, 0);
+	}*/
 
 	protected void jump() {
-		float yMovement = Input.GetAxis ("Jump");
-		transform.position += new Vector3 (0, yMovement, 0);
+		//float yMovement = Input.GetAxis ("Jump");
+		transform.position += new Vector3 (0, .1f, 0);
+
 	}
 }
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,19 +7,22 @@ public class PlayerMove : Move
 	// Use this for initialization
 	void Start ()
 	{
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		move (Input.GetAxis("Horizontal"));
+
+		/*
 		if (Input.GetAxis ("Horizontal") < 0) {
 			moveLeft();
 		}
 
 		if (Input.GetAxis ("Horizontal") > 0) {
 			moveRight();
-		}
+		}*/
 
 		if (Input.GetAxis ("Jump") > 0) {
 			jump();
