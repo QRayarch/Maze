f29a4fa87fb9f73bab130357faa4aa9fcf77809a
unknown <mvm5854@NM2550-25.main.ad.rit.edu>
2015-10-14 16:35:45 -0400

    Grid movement and falling finished. Jumping needs tweaking.

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -13,10 +13,13 @@ public class Move : Stepable {
 
 	private Vector3 endPos;
 
+	protected Grid grid;
+	
 	public virtual void Start()
 	{
 		endPos = transform.position;
 		initMoving = true;
+		grid = GameObject.FindObjectOfType<Grid>();
 	}
 
 	protected void move(float dir)
@@ -26,15 +29,19 @@ public class Move : Stepable {
 
 		if (!moving) {
 
-			if (dir > 0) {
+			int x = (int) (transform.position.x - .5);
+			int y = (int) (transform.position.y - .5);
+
+			if (dir > 0 && !grid.IsGridSpaceCollidable(x + 1, y)) {
 				moving = true;
-				endPos = transform.position + new Vector3 (1, 0, 0);
+				endPos = transform.position + Vector3.right;
 			} 
 
-			else if (dir < 0) {
+			else if (dir < 0 && !grid.IsGridSpaceCollidable(x - 1, y)) {
 				moving = true;
-				endPos = transform.position - new Vector3 (1, 0, 0);
+				endPos = transform.position + Vector3.left;
 			}
+
 		}
 	}
 
@@ -42,7 +49,17 @@ public class Move : Stepable {
 	{
 		if (initMoving) {
 			transform.position = endPos;
+			endPos = transform.position;
+
+			int x = (int) (transform.position.x - .5);
+			int y = (int) (transform.position.y - .5);
+			
+			while(!grid.IsGridSpaceCollidable(x, y - 1) && y >= 0) {
+				endPos += Vector3.down;
+				y--;
+			}
 		}
+
 	}
 
 	protected void jump() {
@@ -52,7 +69,7 @@ public class Move : Stepable {
 		
 		if (!jumping) {
 			jumping = true;
-			endPos=transform.position + new Vector3 (0, 1, 0);
+			endPos=transform.position + new Vector3 (0, 3, 0);
 		}
 
 	}
