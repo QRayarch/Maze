09292696fa0a1642ac386aff15e0ec6dc4b91c49
unknown <afl4947@NMM-23.main.ad.rit.edu>
2015-10-14 16:36:38 -0400

    Added random movement for the AI for testing.

--- Assets/Scripts/AI/AIMove.cs
+++ Assets/Scripts/AI/AIMove.cs
@@ -6,17 +6,49 @@ public class AIMove : Move {
 	[Header("AI Config")]
 	[Range(0.0f, 1.0f)]
 	public float jumpChance = 0.01f;
+	public float turnChance = 0.08f;
 
-	[Header("Weights")]
-	public float seeKey = 1;
+	/*[Header("Weights")]
+	public float seekKey = 1;
+	public float seekMonster = 1;
+	public float seekTrap = 1;*/
+
+	private float seekDoor = 0;
+
+	private Transform trans;
+	private int posX;
+	private int posY;
+
+	//Temp
+	float dir = 1;
 
 	// Use this for initialization
 	void Start () {
 		base.Start();
+
+		trans = transform;
+
+		dir = Random.Range(0, 2) == 0 ? 1: -1;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		posX = (int)(trans.position.x - 0.5f);
+		posY = (int)(trans.position.y - 0.5f);
+
+		float choice = Random.Range(0.0f, 1.0f);
+		if(choice <= jumpChance) {
+			base.jump();
+		} else if (choice - jumpChance <= turnChance) {
+			dir *= -1;
+		}
+
+		//Turn around if there is a wall
+		if(grid.IsGridSpaceCollidable(posX + dir, posY)) {
+			dir *= -1;
+		}
+
+		move(dir);
 	}
 }
