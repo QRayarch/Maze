a3db0cb7b9d8e2827523cfca3c618d3872588ca1
unknown <mvm5854@GDD-15.main.ad.rit.edu>
2015-10-19 17:19:11 -0400

    Started hooking up UI and Enemy Spawner

---
+++ Assets/Scripts/Traps/Move.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//http://answers.unity3d.com/questions/584736/method-for-grid-movement.html
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Move : MonoBehaviour {
+
+	protected Grid grid;
+
+	public int speed=5;
+	public float jumpForce=100f;
+	public bool grounded=false;
+	public Transform groundCheck;
+	float groundRad=.2f;
+	public LayerMask ground;
+	public bool facingRight=true;
+
+
+	float x;
+
+	public virtual void Start()
+	{
+		x = 0;
+		grid = GameObject.FindObjectOfType<Grid>();
+
+	}
+
+	protected void move(float dir, bool jumping)
+	{
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRad, ground);
+
+		if (dir > 0) {
+			x = Vector2.right.x * speed;
+		} else if (dir < 0) {
+
+			x = Vector2.left.x * speed;
+		} else if (dir == 0) {
+			x=0;
+		}
+
+		gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2(x, gameObject.GetComponent<Rigidbody2D> ().velocity.y);
+
+		if(jumping&&grounded){
+			gameObject.GetComponent<Rigidbody2D> ().AddForce(new Vector2(0, jumpForce));
+		}
+	}
+	
+}
---
+++ Assets/Scripts/UICount.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UICount : MonoBehaviour {
+
+	public Text numTraps;
+	int numOfTraps;
+	public int index;
+	public LevelTraps traps;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (traps.trapHolders != null) {
+			numTraps.text = traps.trapHolders [index].numPerLevel.ToString();
+		}
+	}
+}
